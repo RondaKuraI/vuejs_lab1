f03e4c8353cd6e193b017d1e380633bf4b384328
RondaKuraI <johnronda0527@gmail.com>
2023-10-23 18:41:51 +0800

    imported styles

--- src/views/Finter.vue
+++ src/views/Finter.vue
@@ -17,7 +17,10 @@ export default {
     }
 }
 </script>
-
 <style>
-
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap');
+    @import url('https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css');
+    @import url('https://cdnjs.cloudflare.com/ajax/libs/jquery-nice-select/1.1.0/css/nice-select.min.css');
+    @import '@/assets/finter/css/style.css';
+    @import '@/assets/finter/css/responsive.css';
 </style>
